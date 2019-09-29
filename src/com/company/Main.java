26039aa5ce5257799ef7d047220a2c9300907dd4
graytaylor0 <graytaylor0@gmail.com>
2019-09-29 15:10:27 -0500

    Revert "Add files via upload"
    This reverts commit d9476262499a002208b5308ea9a9bf796a7e526e.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,38 +1,24 @@
 package com.company;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
-import project1.antlr4.MyRulesBaseListener;
-import project1.antlr4.rulesLexer;
-import project1.antlr4.rulesParser;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("src/project1/input.txt");
-        Scanner scanner = new Scanner(file);
-        List<String> lines = new ArrayList<>();
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if (line.length() != 0) { lines.add(line); }
-        }
-        for (String line : lines) {
-            CharStream charStream = CharStreams.fromString(line);
-            rulesLexer lexer = new rulesLexer(charStream);
-            CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
-            rulesParser parser = new rulesParser(commonTokenStream);
-            lexer.removeErrorListeners();
-            parser.removeErrorListeners();
-            rulesParser.ProgramContext programContext = parser.program();
-            ParseTreeWalker walker = new ParseTreeWalker();
-            MyRulesBaseListener listener = new MyRulesBaseListener();
-            walker.walk(listener, programContext);
-        }
+
+    public static void main(String[] args) {
+	// write your code here
+        Movie movie1 = new Movie(1, "Spiderman", 2002, "Action");
+        Movie movie2 = new Movie(2, "Batman", 2008, "Action");
+        DBMS database = new DBMS();
+        database.addMovie(movie1);
+        database.addMovie(movie2);
+        System.out.println(database.movies);
+        ArrayList<Movie> spidey = database.getMoviesByName("Spiderman");
+        System.out.println(spidey);
+        /*Person person1 = new CastMember(0, "Taylor", "Lead");
+        Person person2 = new CrewMember(1, "Nick", "Director");
+        movie1.addPerson(person1);
+        movie1.addPerson(person2);
+        movie1.printCastAndCrew();
+        movie1.removePerson(0);*/
     }
 }
